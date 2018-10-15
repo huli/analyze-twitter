7531a92fa4fc43f14c82a16b1521059c6e3ca10c
huli <huli@eitschei.com>
2018-10-15 16:51:04 -0400

    Added basic plotting

--- twitter_listener.py
+++ twitter_listener.py
@@ -2,16 +2,17 @@ import tweepy
 import json
 
 class TwitterStreamListener(tweepy.StreamListener):
-    def __init__(self, api=None):
+    def __init__(self, api=None, number_of_tweets = 100):
         super(TwitterStreamListener, self).__init__()
         self.num_tweets = 0
+        self.number_of_tweets = number_of_tweets
         self.file = open("tweets.txt", "w")
 
     def on_status(self, status):
         tweet = status._json
         self.file.write( json.dumps(tweet) + '\n' )
         self.num_tweets += 1
-        if self.num_tweets < 1000:
+        if self.num_tweets <= self.number_of_tweets:
             return True
         else:
             return False
